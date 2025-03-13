d6cbecf6353f5c8128b21a051b09ac81f839c7b2
Qarib2004 <garibalisultanov@gmail.com>
2025-03-14 00:21:06 +0400

    add card section

--- src/pages/home/home.component.tsx
+++ src/pages/home/home.component.tsx
@@ -1,6 +1,7 @@
 import useHomeStyles from './home.style';
 import OneSection from 'core/layouts/public/components/section-one/one.component';
 import CategoriesSection from 'core/layouts/public/components/categories-section/categories.component';
+import Card from 'core/shared/home-card/card.component';
 
 function HomeComponent() {
     const classes = useHomeStyles();
@@ -10,6 +11,7 @@ function HomeComponent() {
         <div className={classes.home}>
           <OneSection/>
           <CategoriesSection/>
+          <Card/>
         </div>
     );
 }
