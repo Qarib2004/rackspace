69b8f11facbaa07390c061eb1774a09fca3074cb
Qarib2004 <garibalisultanov@gmail.com>
2025-04-05 14:46:14 +0400

    update for deploy

--- src/pages/home/home.style.ts
+++ src/pages/home/home.style.ts
@@ -1,11 +1,11 @@
 import { createUseStyles } from 'react-jss';
 
-const styles = {
+
+
+const useHomeStyles = createUseStyles({
     home: {
         backgroundColor: '#F4F7F6',
     },
-};
-
-const useHomeStyles = createUseStyles(styles);
+});
 
 export default useHomeStyles;
